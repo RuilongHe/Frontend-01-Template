5ebc4e477f3473d84c17566da17677e4a69833f9
RuilongHe <ruilong_he@126.com>
2020-08-19 10:46:21 +0800

    Week19_G20200343040251
    Week19_G20200343040251

---
+++ week19/publish/publish-server-vanilla/index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const fs = require('fs');
+const unzip = require('unzipper');
+
+const server = http.createServer((req, res) => {
+  let writeStream = unzip.Extract({ path: '../server/public/' });
+  req.pipe(writeStream);
+  req.on('end', () => {
+    res.writeHead(200, {
+      'content-Type': 'text/plain',
+    });
+    res.end('okay');
+  });
+});
+
+server.listen(8081);
